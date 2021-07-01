f6a29b92e11ebcee362bf10729fd99f5b71dc9a5
Yury Kasper <kasperyuriu@gmail.com>
2021-07-01 17:14:12 +0300

    Add additional file PhotoNames.swift

---
+++ PhotoMap/Core/Base/Models/PhotoNames.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoNames.swift
+//  PhotoMap
+//
+//  Created by Dzmitry Makarevich on 6/28/21.
+//
+
+import Foundation
+
+enum PhotoNames: String {
+    case category, date, description, hashtags, images, point
+}
